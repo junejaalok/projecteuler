ab7a3c3b0087679cc93d856dde91c3d298b635a1
Alok Juneja <junejaalok@gmail.com>
2014-10-24 07:55:09 +0200

    20141024_1

--- python/lattice-path.py
+++ python/lattice-path.py
@@ -1,33 +1,13 @@
-import itertools
-
-row=20
-col=20
-right=1
-down=row+1
-cnt =0
-#print range(1,22,20)
-for i in itertools.product(xrange(1,22,20),repeat=40):
-	#print type(list(i))
-
-	#print j, " : ", sum(list(i)), " : ",map(sum,i)
-	#j += 1
-	#print i
-	if sum(list(i)) == 440:
-		cnt +=1
-	#	print i
-
-print cnt
-
-'''
 def lpath (a,b):
-	right=a-1
-	down=b+1
-	last=2*(a*b)
-	for i in range(1,4,2):
-		summe=0
-		while sum <= last:
-			for j in range
+	row=a
+	col=b
+	n=row+col
+	print factorial(n)/(factorial(n-row)*factorial(row))
 
-
-lpath(2,2)
-'''
+def factorial(n):
+	if n==1:
+		return n
+	else:
+		return n*factorial(n-1)
+	
+lpath(20,20)
